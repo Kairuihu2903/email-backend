78da0fe93c34006365f5c5210752804502bb5d78
kairui2903 <kairui0619@gmail.com>
2024-06-05 21:56:40 -0400

    create injector forback end

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
+  getData(): string {
     return 'Hello World!';
+  }
+}
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getData(): string {
+    return this.appService.getData();
   }
 }
